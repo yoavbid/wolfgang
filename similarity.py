b0746a141c53c0dbd556a704ac8c96188dbd8896
Yoav Biderman <yoav.biderman@joytunes.com>
2023-06-22 11:55:57 +0300

    add chat history to semantic search
    Signed-off-by: Yoav Biderman <yoav.biderman@joytunes.com>

---
+++ similarity.py
@@ -0,0 +1,60 @@
+import faiss
+import pickle
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+from simply_tutor.src import simply_tutor_path
+from langchain.document_loaders import TextLoader
+
+
+
+def generate_similarity_index():
+    data_files_list= list((simply_tutor_path / "training_data/").glob("**/*.srt"))
+    data_files_list.extend(list((simply_tutor_path / "training_data/").glob("**/*.txt")))
+
+    documents = []
+    for data_file in data_files_list:
+        loader = TextLoader(data_file)
+        documents.extend(loader.load())
+        
+    textSplitter = CharacterTextSplitter(chunk_size=2000, separator="\n")
+
+    docs = textSplitter.split_documents(documents)
+
+    store = FAISS.from_documents(docs, OpenAIEmbeddings())
+    # faiss.write_index(store.index, str(simply_tutor_path / "training_data" / "app_context.index"))
+    
+    # docs = store.similarity_search("middle c")
+
+    # with open(simply_tutor_path / "training_data" / "faiss.pkl", "wb") as f:
+    #     pickle.dump(store, f)
+        
+    store.save_local(simply_tutor_path / "faiss_index")
+    
+def load_similarity_index():
+    store = FAISS.load_local(simply_tutor_path / "faiss_index", OpenAIEmbeddings())
+        
+    return store
+
+
+def test_similarity_index(text):
+    index = faiss.read_index(str(simply_tutor_path / "training_data" / "app_context.index"))
+
+    with open(simply_tutor_path / "training_data" / "faiss.pkl", "rb") as f:
+        store = pickle.load(f)
+
+    store.index = index
+
+    docs = store.similarity_search(text, k=3) #similarity_search(text)
+    for i, doc in enumerate(docs):
+        print('%d. ' % i, doc.page_content, '\n\n')
+        
+        
+def main():
+    generate_similarity_index()
+    
+    test_similarity_index('What other songs can I practice after Somewhere Over the Rainbow?')
+    
+        
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,97 +1,71 @@
 import logging
 import time
-import faiss
-import pickle
 from langchain import LLMChain
-import pickle
 from langchain.prompts import Prompt
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts.chat import (
-  ChatPromptTemplate,
-  SystemMessagePromptTemplate,
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
 )
+from similarity import load_similarity_index
 
 
-def get_chat_chain_and_store(prompt_path,
-                             model='gpt-3.5-turbo',
-                             temperature=0.4,
-                             logger=None):
-  if logger is None:
-    logger = get_default_logger()
+def get_chat_chain_and_store(prompt_path, model = 'gpt-3.5-turbo', temperature=0.4, logger=None):
+    if logger is None:
+        logger = get_default_logger()
+    
+    store = load_similarity_index()
+    
+    with open(prompt_path, "r") as f:
+        promptTemplate = f.read()
 
-  store = get_index_store()
+        system_message_prompt = SystemMessagePromptTemplate.from_template(
+            template=promptTemplate, input_variables=["history", "context", "recent_level", "input"])
 
-  with open(prompt_path, "r") as f:
-    promptTemplate = f.read()
+    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt])
 
-    system_message_prompt = SystemMessagePromptTemplate.from_template(
-      template=promptTemplate,
-      input_variables=["history", "context", "recent_level", "input"])
+    chat = ChatOpenAI(temperature=temperature, model=model)
 
-  chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt])
+    chain = LLMChain(llm=chat, prompt=chat_prompt)
+    
+    return chain, store
 
-  chat = ChatOpenAI(temperature=temperature, model=model)
+def ask_question(question, history, chain, store, recent_level=None, num_contexts=8):
+    try:
+        history_for_context = history[-5:]
+    except IndexError:
+        history_for_context = history
+        
+    search_text = question + ' history: ' + ' '.join(history_for_context)
+    if recent_level is not None:
+        search_text += ' context: ' + recent_level
+        
+    docs = store.similarity_search(search_text, k=num_contexts)
+    contexts = []
+    contexts_no_index = []
+    for i, doc in enumerate(docs):
+        contexts.append(f"Context {i}:\n{doc.page_content}")
+        contexts_no_index.append(doc.page_content)
 
-  chain = LLMChain(llm=chat, prompt=chat_prompt)
-
-  return chain, store
-
-
-def ask_question(question,
-                 history,
-                 chain,
-                 store,
-                 recent_level=None,
-                 num_contexts=8):
-  search_text = question
-  if recent_level is not None:
-    search_text += ' context: ' + recent_level
-
-  docs = store.similarity_search(search_text, k=num_contexts)
-  contexts = []
-  contexts_no_index = []
-  for i, doc in enumerate(docs):
-    contexts.append(f"Context {i}:\n{doc.page_content}")
-    contexts_no_index.append(doc.page_content)
-
-  answer = chain.run(input=question,
-                     context="\n\n".join(contexts),
-                     recent_level=recent_level,
-                     history=history)
-
-  history.append(f"Human: {question}")
-  history.append(f"Bot: {answer}")
-
-  return answer, contexts_no_index
-
-
-def get_index_store():
-  index = faiss.read_index("app_context.index")
-
-  with open("faiss.pkl", "rb") as f:
-    store = pickle.load(f)
-
-  store.index = index
-
-  return store
-
+    answer = chain.run(input=question, context="\n\n".join(contexts), recent_level=recent_level, history=history)
+    
+    history.append(f"Human: {question}")
+    history.append(f"Bot: {answer}")
+    
+    return answer, contexts_no_index
 
 def setup_logger(name):
-  logger = logging.getLogger()
-  logger.level = logging.INFO
-  logger.addHandler(logging.StreamHandler())
-  time_str = time.strftime("%Y%m%d-%H%M%S")
-  logger.addHandler(
-    logging.FileHandler(simply_tutor_path / ("logs/%s_%s.txt" %
-                                             (name, time_str)),
-                        mode="w"))
-
-  return logger, time_str
-
+    logger = logging.getLogger()
+    logger.level = logging.INFO
+    logger.addHandler(logging.StreamHandler())
+    time_str = time.strftime("%Y%m%d-%H%M%S")
+    logger.addHandler(logging.FileHandler(simply_tutor_path / ("logs/%s_%s.txt" % (name, time_str)), mode="w"))
+    
+    return logger, time_str
 
 def get_default_logger():
-  logger = logging.getLogger()
-  logger.level = logging.INFO
-  logger.addHandler(logging.StreamHandler())
-
-  return logger
+    logger = logging.getLogger()
+    logger.level = logging.INFO
+    logger.addHandler(logging.StreamHandler())
+    
+    return logger
